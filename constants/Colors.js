1cffd07b794197391f0e82a5286d8fb3572d1f8f
Murilo Henrique <oofleaoo@gmail.com>
2019-01-23 00:24:43 -0200

    Constants Added

---
+++ constants/Colors.js
@@ -0,0 +1,17 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: '#fff',
+  tabBar: '#00a2ae',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  fontColor: '#00a2ae',
+  headerColor: '#00a2ae',
+  buttonSubmit: '#f9a138'
+};
