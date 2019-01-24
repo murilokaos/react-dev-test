e9e2897853f2e414948a8f4944bf854607cd804f
Murilo Henrique <oofleaoo@gmail.com>
2019-01-23 23:42:22 -0200

    Final Version

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,48 +1,109 @@
 import React from 'react';
-import {
-  Image,
-  StyleSheet,
-  Text,
-  Button,
-  View,
-  ButtonGroup
-} from 'react-native';
-import { Constants } from 'expo'
-import { SafeAreaView } from 'react-navigation'
+import { Alert, Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import Colors from '../constants/Colors';
-import Layout from '../constants/Layout'
+import t from 'tcomb-form-native'; // 0.6.9
+import { ScrollView } from 'react-native-gesture-handler';
+import stylesheet from '../stylesheet/stylesheet'
+
+const Form = t.form.Form;
+
+const Person = t.struct({
+  email: t.String,              
+  senha: t.String
+});
+
+const options = {
+  stylesheet: stylesheet,
+  fields: {
+    email: {
+      label: 'E-mail',
+    },
+    senha: {
+      password:true,
+      secureTextEntry:true,
+    }
+  }
+};
 
 export default class LoginScreen extends React.Component {
+  state = {
+    value: null
+  }
   static navigationOptions = {
-    tabBarLabel: 'Login',
+    title: 'Login',
   };
+
+  onChange(value) {
+    this.setState({ value });
+  };
+
+  clearForm() {
+    this.setState({ value: null });
+  };
+
+  notifyLogin(){
+    Alert.alert(
+      'Login',
+      'Login efetuado com sucesso!',
+      [
+        {text: 'Cancelar', style: 'cancel'},
+        {text: 'OK'},
+      ],
+      { cancelable: false }
+    )
+  }
+
+  notifyButton(value){
+    Alert.alert(
+      'Você Clicou em:',
+      value === 'signin' ? 'Fazer Login' : (value === 'signup' ? 'Criar Conta' : 'Esqueci minha senha'),
+      [
+        {text: 'Cancelar', style: 'cancel'},
+        {text: 'OK'},
+      ],
+      { cancelable: false }
+    )
+  }
+
+  onLogin = () => {
+    let value = this.refs.form.getValue();
+    if (value) {
+      this.clearForm();
+      this.notifyLogin();
+    }
+  };
+
+  onPress = (value) => {
+    this.notifyButton(value);
+  }
 
   render() {
     
     return (
-      <SafeAreaView
-        style={styles.container}
-      >
+      <ScrollView>
       <View style={styles.banner}> 
         <Image source={require('../assets/images/navLogo.png')} style={styles.image} />
           <View>
-            <Text style={styles.text}>Obrigado por fazer parte da nossa rede de negócios.</Text>
+            <Text style={[styles.text, styles.noticeText, styles.bold]}>Obrigado por fazer parte da nossa rede de negócios.</Text>
           </View>
       </View>
       <View style={styles.buttons}>
-        <Button style={styles.button} title="FAZER LOGIN" onPress={() => {  }}></Button>
-        <Button style={styles.button} title="CRIAR CONTA" onPress={() => {  }}></Button>
-        <ButtonGroup
-        selectedBackgroundColor="pink"
-        onPress={this.updateIndex}
-        selectedIndex={this.state.index}
-        buttons={['FAZER LOGIN', 'CRIAR CONTA']}
-        containerStyle={{height: 30}} />
+        <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 'signin')} underlayColor='#00a2a102'>
+          <Text style={[styles.buttonText, styles.noticeText]} accessibilityLabel="Clique aqui para fazer login">FAZER LOGIN</Text>
+        </TouchableHighlight >
+        <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 'signup')} underlayColor='#00a2a102'>
+          <Text style={[styles.buttonText, styles.noticeText]} accessibilityLabel="Clique aqui para criar uma conta">CRIAR CONTA</Text>
+        </TouchableHighlight>
       </View>
-      <View>
-          <Text style={styles.text} color={Colors.fontColor}>Insira aqui seus dados para acessar sua conta</Text>
+      <View style={styles.form}>
+          <Text style={[styles.formText, styles.fontColor, styles.bold]}>Insira aqui seus dados para acessar sua conta</Text>
+        <Form ref="form" type={Person} options={options} style={styles.form} value={this.state.value} onChange={this.onChange.bind(this)}/>
+        <Text style={[styles.formText, styles.fontColor]} onPress={this.onPress.bind(this, 'lost')}>ESQUECI MINHA SENHA :/</Text>
+        <TouchableHighlight style={styles.buttonLogin} onPress={this.onLogin} underlayColor='#99d9f4'>
+          <Text style={[styles.buttonText, styles.noticeText]}>Entrar</Text>
+        </TouchableHighlight>
       </View>
-    </SafeAreaView>
+    </ScrollView>
     );
   }
 }
@@ -57,7 +118,7 @@ const styles = StyleSheet.create({
   banner: {
     flexDirection: 'row',
     alignItems: 'center',
-    height: 160,
+    height: 130,
   },
   image: {
     resizeMode: 'cover',
@@ -67,21 +128,54 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   buttons: {
+    flexGrow: 0,
     flexDirection: 'row',
     backgroundColor: Colors.headerColor,
-    justifyContent: 'space-around',
   },
   button: {
-    flex: 0.9
+    flexGrow: 1,
+    justifyContent: 'center',
+    height: 50,
+    backgroundColor: Colors.headerColor,
   },
   text: {
-    fontWeight: 'bold',
     fontSize: 16,
-    color: Colors.noticeText,
     textAlign: 'center',
-    paddingLeft: 70,
-    paddingRight: 70,
+    paddingHorizontal: 70,
     paddingTop: 50
   },
-  
+  bold: {
+    fontWeight: 'bold',
+  },
+  noticeText: {
+    color: Colors.noticeText,
+  },
+  fontColor: {
+    color: Colors.fontColor,
+  },
+  formText: {
+    fontSize: 16,
+    textAlign: 'center',
+    paddingHorizontal: 70,
+    paddingVertical: 20
+  },
+  buttonText: {
+    fontSize: 16,
+    alignSelf: 'center',
+    fontWeight: 'bold'
+  },
+  buttonLogin: {
+    height: 50,
+    backgroundColor: '#f9a138',
+    borderColor: '#f9a138',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  form: {
+    paddingHorizontal: 5,
+    color: '#CCC'
+  },
 });
